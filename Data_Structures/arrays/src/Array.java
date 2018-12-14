3161e8d5597ae916c4561a89204093d494122279
George Koubbe <gkoubbe@outlook.com>
2018-12-13 22:27:20 -0500

    Trying this out

---
+++ Data_Structures/arrays/src/Array.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+    }
+
+}
