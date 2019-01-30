9a1fb2388d9e116c15fe39f585cfc3da9d67440d
George Koubbe <gkoubbe@outlook.com>
2019-01-29 19:41:38 -0500

    array creation

--- Data_Structures/arrays/src/Array.java
+++ Data_Structures/arrays/src/Array.java
@@ -9,6 +9,15 @@ import java.util.regex.*;
 public class Array {
 
     public static void main(String[] args) {
+        
+        int[] arr;
+        arr = new int[5];
+
+        for (int i = 0; i < 5; i++){
+            arr[i] = i;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
     }
 
